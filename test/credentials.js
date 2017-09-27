3f7aaab13c02e3cba80ab04d460bc4c665abcb41
hiryus <hiryus.sha@gmail.com>
2017-09-27 13:12:38 +0000

    Removed old credentials in comment.

--- test/credentials.js
+++ test/credentials.js
@@ -5,12 +5,3 @@ module.exports = {
     hostname: 'server1.screepspl.us',
     port:     443,
 };
-/*
-module.exports = {
-    username: 'ryanis.shadow@gmail.com',
-    password: '61f2d5454d9ea0a131e9ac319b46aadb712563a06e9d670846f24aec1063e5ce', // official
-    protocol: 'https',
-    hostname: 'screeps.com',
-    port:     443,
-};
-*/
